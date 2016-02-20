fee99766494874c49ef60dac9262a6d6cb655f9c
Young Park <i.make.seyoung@gmail.com>
2016-02-20 11:39:30 +0200

    Create project & podfile, gitignore

---
+++ universal-file-transfer/universal-file-transfer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  universal-file-transfer
+//
+//  Created by YOUNG on 2/20/16.
+//  Copyright © 2016 Mobile Mob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
